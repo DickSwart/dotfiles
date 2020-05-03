b1a76ac7a27915ea19c64b784a3f33e51cad95e6
Dick Swart <dick@swart.ninja>
2020-05-04 09:52:34 +1200

    added python

---
+++ python/path.zsh
@@ -0,0 +1,7 @@
+# #readline
+# export LDFLAGS="-L/usr/local/opt/readline/lib"
+# export CPPFLAGS="-I/usr/local/opt/readline/include"
+
+# #sqlite
+# export LDFLAGS="-L/usr/local/opt/sqlite/lib"
+# export CPPFLAGS="-I/usr/local/opt/sqlite/include"
